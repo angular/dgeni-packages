a39a8e1484ca73b60b45cb62b5f46746a0fd4aa0
Peter Bacon Darwin <pete@bacondarwin.com>
2014-02-27 14:05:18 +0000

    test(filter-ngdocs): use TagCollection rather than MockTags

--- ngdoc/spec/processors/filter-ngdocs.spec.js
+++ ngdoc/spec/processors/filter-ngdocs.spec.js
@@ -1,20 +1,30 @@
 var plugin = require('../../processors/filter-ngdocs');
-var MockTags = require('../MockTags');
+var TagCollection = require('../../../jsdoc/lib/TagCollection');
+var Tag = require('../../../jsdoc/lib/Tag');
+
+function addTag(doc, name, description) {
+  doc.tags.addTag({ tagDef: {name: name}, name: name, description: description});
+}
 
 describe("filter-ngdocs doc-processor plugin", function() {
   it("should only return docs that have the ngdoc tag", function() {
-    var docs = [
-      { tags: new MockTags({ ngdoc: 'a' }) },
-      { tags: new MockTags({ other: 'b' }) },
-      { tags: new MockTags({ ngdoc: 'c' , other: 'd' }) },
-      { tags: new MockTags([]) }
-    ];
+
+    var doc1 = { tags: new TagCollection() };
+    addTag(doc1, 'ngdoc', 'a');
+
+    var doc2 = { tags: new TagCollection() };
+    addTag(doc2, 'other', 'b');
+
+    var doc3 = { tags: new TagCollection() };
+    addTag(doc3, 'ngdoc', 'c');
+    addTag(doc3, 'other', 'd');
+
+    var doc4 = { tags: new TagCollection() };
+
+    var docs = [ doc1, doc2, doc3, doc4 ];
 
     var filteredDocs = plugin.process(docs);
 
-    expect(filteredDocs).toEqual([
-      { tags: new MockTags({ ngdoc: 'a' }) },
-      { tags: new MockTags({ ngdoc: 'c' , other: 'd' }) }
-    ]);
+    expect(filteredDocs).toEqual([doc1, doc3]);
   });
 });
