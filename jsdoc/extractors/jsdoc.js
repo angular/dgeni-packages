50ad83d84adaec1bb1872bb6ba91c6a418546abf
Peter Bacon Darwin <pete@bacondarwin.com>
2014-03-07 04:10:27 +0000

    fix(jsdoc extractor): ignore non-jsdoc comments

--- jsdoc/extractors/jsdoc.js
+++ jsdoc/extractors/jsdoc.js
@@ -6,18 +6,29 @@ module.exports = {
   pattern: /\.js$/,
   processFile: function(filePath, contents, basePath) {
 
-    var docFromComment = function(comment) {
+    var ast = jsParser.parse(contents, {
+      loc: true,
+      range: true,
+      comment: true
+    });
 
-      if ( comment.type === 'Block' && comment.value.charAt(0) === '*' ) {
-        // We have a jsdoc comment (i.e. starting with /** ) - the parser strips off the first "/*"
-        // so we are just left with a single asterisk
-        
+    return _(ast.comments)
+
+      .filter(function(comment) {
+        // To test for a jsdoc comment (i.e. starting with /** ), we need to check for a leading
+        // star since the parser strips off the first "/*"        
+        return comment.type === 'Block' && comment.value.charAt(0) === '*';
+      })
+
+      .map(function(comment) {
+
         // Strip off any leading stars
         text = comment.value.replace(LEADING_STAR, '');
 
         // Trim off leading and trailing whitespace
         text = text.trim();
 
+        // Create a doc from this comment
         return {
           fileType: 'js',
           startingLine: comment.loc.start.line,
@@ -26,14 +37,9 @@ module.exports = {
           basePath: basePath,
           content: text
         };
-      }
-    };
 
-    var ast = jsParser.parse(contents, {
-      loc: true,
-      comment: true
-    });
+      })
 
-    return _.map(ast.comments, docFromComment);
+      .value();
   }
 };
--- jsdoc/spec/extractors/jsdoc.spec.js
+++ jsdoc/spec/extractors/jsdoc.spec.js
@@ -1,4 +1,4 @@
-var jsExtractor = require('../../extractors/jsdoc');
+var extractor = require('../../extractors/jsdoc');
 
 var srcJsContent = require('./_test-data/srcJsFile.js');
 var docsFromJsContent = require('./_test-data/docsFromJsFile');
@@ -6,17 +6,22 @@ var docsFromJsContent = require('./_test-data/docsFromJsFile');
 describe("js doc extractor", function() {
   describe("pattern", function() {
     it("should only match js files", function() {
-      expect(jsExtractor.pattern.test('abc.js')).toBeTruthy();
-      expect(jsExtractor.pattern.test('abc.ngdoc')).toBeFalsy();
+      expect(extractor.pattern.test('abc.js')).toBeTruthy();
+      expect(extractor.pattern.test('abc.ngdoc')).toBeFalsy();
     });
   });
   describe("process", function() {
     it('should return a collection of documents extracted from the file', function() {
-      var docs = jsExtractor.processFile('some/file.js', srcJsContent);
-      docs.length = 3;
+      var docs = extractor.processFile('some/file.js', srcJsContent);
+      expect(docs.length).toEqual(3);
       expect(docs[0]).toEqual(jasmine.objectContaining(docsFromJsContent[0]));
       expect(docs[1]).toEqual(jasmine.objectContaining(docsFromJsContent[1]));
       expect(docs[2]).toEqual(jasmine.objectContaining(docsFromJsContent[2]));
+    });
+
+    it("should ignore non-jsdoc comments", function() {
+      var docs = extractor.processFile('some/file.js', '/** Some jsdoc comment */\n// A line comment\n\/* A non-jsdoc block comment*/');
+      expect(docs.length).toEqual(1);
     });
   });
 });
