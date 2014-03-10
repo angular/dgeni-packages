8ddcf6470e3767f63c4705f098078ce71c5fa817
Andy Joslin <andytjoslin@gmail.com>
2014-03-10 07:49:48 -0600

    feat(api-docs): add configuration for paths

--- ngdoc/spec/processors/api-docs.spec.js
+++ ngdoc/spec/processors/api-docs.spec.js
@@ -1,8 +1,36 @@
-var processor = require('../../processors/api-docs');
+var rewire = require('rewire');
+var processor = rewire('../../processors/api-docs');
 var PartialNames = require('../../utils/partial-names').PartialNames;
 var Config = require('dgeni/lib/utils/config').Config;
 var di = require('di');
 var _ = require('lodash');
+
+describe("api-docs config", function() {
+  it("should provide defaults for its options", function() {
+    
+    var config = _.extend(Config);
+    config.set('rendering.contentsFolder', 'partials');
+    processor.init(config, new di.Module());
+
+    expect(processor.__get__('options')).toEqual({
+      outputPath: '${area}/${module}/${docType}/${name}.html',
+      path: '${area}/${module}/${docType}/${name}',
+      moduleOutputPath: '${area}/${name}/index.html',
+      modulePath: '${area}/${name}'
+    });
+  });
+
+  it("should let us override the options", function() {
+    var config = _.extend(Config);
+    config.set('rendering.contentsFolder', 'partials');
+    
+    config.set('processing.api-docs.path', 'XXX');
+    processor.init(config, new di.Module());
+    
+    expect(processor.__get__('options.path')).toEqual('XXX');
+    expect(processor.__get__('options.moduleOutputPath')).toEqual('${area}/${name}/index.html');
+  });
+});
 
 describe("api-docs processor", function() {
   var config;
