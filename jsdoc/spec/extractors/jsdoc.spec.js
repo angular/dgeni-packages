a517b4d5bc0c5f6baeab922eb92ced019d2f0998
Peter Bacon Darwin <pete@bacondarwin.com>
2014-03-21 15:10:10 +0000

    test(jsdoc/jsdoc-extractor): test whitespace/star stripping

--- jsdoc/spec/extractors/jsdoc.spec.js
+++ jsdoc/spec/extractors/jsdoc.spec.js
@@ -4,13 +4,17 @@ var srcJsContent = require('./_test-data/srcJsFile.js');
 var docsFromJsContent = require('./_test-data/docsFromJsFile');
 
 describe("js doc extractor", function() {
+
   describe("pattern", function() {
     it("should only match js files", function() {
       expect(extractor.pattern.test('abc.js')).toBeTruthy();
       expect(extractor.pattern.test('abc.ngdoc')).toBeFalsy();
     });
   });
+
+
   describe("process", function() {
+
     it('should return a collection of documents extracted from the file', function() {
       var docs = extractor.processFile('some/file.js', srcJsContent);
       expect(docs.length).toEqual(3);
@@ -19,10 +23,18 @@ describe("js doc extractor", function() {
       expect(docs[2]).toEqual(jasmine.objectContaining(docsFromJsContent[2]));
     });
 
+
+    it("should strip off the leading whitespace/stars from each line of the comments", function() {
+      var docs = extractor.processFile('some/file.js', '/** abc  \n  * other stuff  \n\t\t*last line.\n*/\n');
+      expect(docs[0].content).toEqual('abc  \nother stuff  \nlast line.');
+    });
+
+
     it("should ignore non-jsdoc comments", function() {
       var docs = extractor.processFile('some/file.js', '/** Some jsdoc comment */\n// A line comment\n\/* A non-jsdoc block comment*/');
       expect(docs.length).toEqual(1);
     });
+
 
     it("should find the next code item following the comment and attach it to the doc", function() {
       var docs = extractor.processFile('some/file.js', srcJsContent);
