1115c431eedbf22f90f98cfd81861f86ecce1b1a
Peter Bacon Darwin <pete@bacondarwin.com>
2014-04-09 13:37:42 +0100

    feat(jsdoc/name-from-code): extract the name of the doc from the code

--- jsdoc/index.js
+++ jsdoc/index.js
@@ -1,20 +1,21 @@
 module.exports = function(config) {
 
   config.append('source.extractors', require('./extractors/jsdoc'));
-  
+
   config.append('processing.processors', [
     require('./processors/doc-extractor'),
     require('./processors/tag-parser'),
     require('./processors/tag-extractor'),
+    require('./processors/name-from-code'),
     require('./processors/compute-path'),
     require('./processors/nunjucks-renderer'),
     require('./processors/escaped-comments'),
     require('./processors/inline-tags'),
     require('./processors/write-files')
   ]);
-  
+
   config.append('processing.tagDefinitions', require('./tag-defs'));
-  
+
   config.append('rendering.filters', [
     require('./rendering/filters/dash-case'),
     require('./rendering/filters/first-line'),
---
+++ jsdoc/processors/name-from-code.js
@@ -0,0 +1,49 @@
+var _ = require('lodash');
+var log = require('dgeni').log;
+
+/**
+ * Recurse down the code AST node that is associated with this doc for a name
+ * @param  {Object} node The esprima node information for the code to find the name of
+ * @return {String}      The name of the code or null if none found.
+ */
+function findCodeName(node) {
+  var match;
+  switch(node.type) {
+    case 'FunctionDeclaration':
+      return node.id && node.id.name;
+    case 'ExpressionStatement':
+      return findCodeName(node.expression);
+    case 'AssignmentExpression':
+      return findCodeName(node.right) || findCodeName(node.left);
+    case 'FunctionExpression':
+      return node.id && node.id.name;
+    case 'MemberExpression':
+      return findCodeName(node.property);
+    case 'Identifier':
+      return node.name;
+    case 'ReturnStatement':
+      return findCodeName(node.argument);
+    case 'Property':
+      return findCodeName(node.value) || findCodeName(node.key);
+    case 'ObjectExpression':
+      return null;
+    case 'Program':
+      return null;
+    default:
+      log.warn('HELP!');
+      log.warn(node);
+      return null;
+  }
+}
+
+module.exports = {
+  name: 'name-from-code',
+  runAfter: ['processing-docs'],
+  runBefore: ['docs-processed'],
+  process: function(docs) {
+    _.forEach(docs, function(doc) {
+      doc.name = doc.name || findCodeName(doc.code.node);
+    });
+    return docs;
+  }
+};
