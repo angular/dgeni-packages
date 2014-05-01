1d2e689e04909ad5d397faa418a78a28e611c850
Peter Bacon Darwin <pete@bacondarwin.com>
2014-05-01 10:49:27 +0100

    refact(tagParser): move into its own processor

--- jsdoc/spec/processors/tagParser.spec.js
+++ jsdoc/spec/processors/tagParser.spec.js
@@ -1,23 +1,26 @@
-var tagParserFactory = require('../../lib/tagParser');
+var tagParserProcessor = require('../../processors/tagParser');
+var tagParserFactory = tagParserProcessor.exports.tagParser[1];
 
-describe("tagParser", function() {
+describe("tagParser helper", function() {
   describe("tagParserFactory", function() {
-    it("should accept a set of tag-definitions and return a configured tagParser", function() {
-      var tagDefinitions = [];
-      var tagParser = tagParserFactory(tagDefinitions);
+    it("should accept a map of tag-definitions and return a configured tagParser", function() {
+      var tagDefMap = {};
+      var tagParser = tagParserFactory(tagDefMap);
       expect(tagParser).toEqual(jasmine.any(Function));
     });
   });
 
   describe("tagParser", function() {
+
+    var tagDefMap = {
+      'id':          { name: 'id' },
+      'description': { name: 'description' },
+      'param':       { name: 'param' },
+      'other-tag':   { name: 'other-tag', ignore: true }
+    };
+
     it("should only return tags that are not ignored", function() {
-      var tagDefinitions = [
-        { name: 'id' },
-        { name: 'description' },
-        { name: 'param' },
-        { name: 'other-tag', ignore: true }
-      ];
-      var tagParser = tagParserFactory(tagDefinitions);
+      var tagParser = tagParserFactory(tagDefMap);
       var content = 'Some initial content\n@id some.id\n' +
                     '@description Some description\n@other-tag Some other tag\n' +
                     '@param some param\n@param some other param';
@@ -39,13 +42,8 @@ describe("tagParser", function() {
       );
     });
 
-    it("should cope with tags that have no description", function() {
-      var tagDefinitions = [
-        { name: 'id' },
-        { name: 'description' },
-        { name: 'param' }
-      ];
-      var tagParser = tagParserFactory(tagDefinitions);
+    it("should cope with tags that have no 'description'", function() {
+      var tagParser = tagParserFactory(tagDefMap);
       var content = '@id\n@description some description';
       var tags = tagParser(content, 123);
       expect(tags.tags[0]).toEqual(jasmine.objectContaining({ tagName: 'id', description: '' }));
@@ -53,7 +51,7 @@ describe("tagParser", function() {
     });
 
     it("should cope with empty content or no known tags", function() {
-      var tagParser = tagParserFactory([]);
+      var tagParser = tagParserFactory({});
       expect(function() {
         tagParser('', 123);
       }).not.toThrow();
@@ -65,7 +63,7 @@ describe("tagParser", function() {
 
 
     it("should ignore @tags inside back-ticked code blocks", function() {
-      var tagParser = tagParserFactory([ { name: 'a' }, { name: 'b' }]);
+      var tagParser = tagParserFactory({a: { name: 'a' }, b: { name: 'b' }});
       var tags = tagParser(
         '@a some text\n\n' +
         '```\n' +
