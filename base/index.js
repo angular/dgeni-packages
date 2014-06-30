f87d00b29caa2bde711f08937aa88d6dac7b8139
Peter Bacon Darwin <pete@bacondarwin.com>
2014-06-30 20:57:12 +0100

    refact(base): update to no-config

--- base/index.js
+++ base/index.js
@@ -1,32 +1,28 @@
 var Package = require('dgeni').Package;
 
+// Define the `base` package
 module.exports = new Package('base')
 
-// Create a set of pseudo processors that act as markers to help position real
-// processors at the right place in the pipeline
-.processor('reading-files')
-.processor('files-read', { $runAfter: ['reading-files'] })
-.processor('processing-docs', { $runAfter: ['files-read'] })
-.processor('docs-processed', { $runAfter: ['processing-docs'] })
-.processor('adding-extra-docs', { $runAfter: ['docs-processed'] })
-.processor('extra-docs-added', { $runAfter: ['adding-extra-docs'] })
-.processor('rendering-docs', { $runAfter: ['extra-docs-added'] })
-.processor('docs-rendered', { $runAfter: ['rendering-docs'] })
-.processor('writing-files', { $runAfter: ['docs-rendered'] })
-.processor('files-written', { $runAfter: ['writing-files'] })
+// A set of pseudo processors that act as markers to help position real processors at the right
+// place in the pipeline
+.processor({ name: 'reading-files' })
+.processor({ name: 'files-read', $runAfter: ['reading-files'] })
+.processor({ name: 'processing-docs', $runAfter: ['files-read'] })
+.processor({ name: 'docs-processed', $runAfter: ['processing-docs'] })
+.processor({ name: 'adding-extra-docs', $runAfter: ['docs-processed'] })
+.processor({ name: 'extra-docs-added', $runAfter: ['adding-extra-docs'] })
+.processor({ name: 'rendering-docs', $runAfter: ['extra-docs-added'] })
+.processor({ name: 'docs-rendered', $runAfter: ['rendering-docs'] })
+.processor({ name: 'writing-files', $runAfter: ['docs-rendered'] })
+.processor({ name: 'files-written', $runAfter: ['writing-files'] })
 
-// Add in the real processors for this package
-.processor('readFiles', require('./processors/read-files'))
-.processor('renderDocs', require('./processors/render-docs'))
-.processor('unescapeComments', require('./processors/unescape-comments'))
-.processor('writeFiles', require('./processors/write-files'))
+// Real processors for this package
+.processor(require('./processors/read-files'))
+.processor(require('./processors/render-docs'))
+.processor(require('./processors/unescape-comments'))
+.processor(require('./processors/write-files'))
 
 // Helper services
-.factory('templateFinder', require('./services/templateFinder'))
-.factory('templateFolders', function() { return []; })
-.factory('templatePatterns', function() { return []; })
-
-
-.config(function(dgeni) {
-  dgeni.basePath = process.cwd();
-});
+.factory(require('./services/templateFinder'))
+.factory(require('./services/templateFolders'))
+.factory(require('./services/templatePatterns'));
