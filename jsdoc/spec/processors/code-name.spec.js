8c6482f81fb00d09f2558f4be8ed106d53fec6d9
Peter Bacon Darwin <pete@bacondarwin.com>
2014-07-02 17:49:26 +0100

    refact(code-name): update tests to no-config

--- jsdoc/spec/processors/code-name.spec.js
+++ jsdoc/spec/processors/code-name.spec.js
@@ -1,18 +1,22 @@
-var processor = require('../../processors/code-name');
+var codeNameProcessorFactory = require('../../processors/code-name');
 var jsParser = require('esprima');
 
-describe('code-name doc processor', function() {
+var mockLog = jasmine.createSpyObj('log', ['error', 'warn', 'info', 'debug', 'silly']);
+
+ddescribe('code-name doc processor', function() {
   it("should understand CallExpressions", function() {
+    var processor = codeNameProcessorFactory(mockLog);
     var ast = jsParser.parse('(function foo() { })()');
     var doc = { codeNode: { node: ast } };
-    processor.process([doc]);
+    processor.$process([doc]);
     expect(doc.codeName).toEqual('foo');
   });
 
   it("should understand ArrayExpressions", function() {
+    var processor = codeNameProcessorFactory(mockLog);
     var ast = jsParser.parse("$CompileProvider.$inject = ['$provide', '$$sanitizeUriProvider'];");
     var doc = { codeNode: { node: ast } };
-    processor.process([doc]);
+    processor.$process([doc]);
     expect(doc.codeName).toEqual('$inject');
   });
 
