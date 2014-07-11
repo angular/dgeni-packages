68686c975d0be5bf6c7d15e265265141fc8b8ca0
Peter Bacon Darwin <pete@bacondarwin.com>
2014-07-11 19:34:41 +0100

    refact(parseTagsProcessor): update to no-config

--- jsdoc/processors/parse-tags.js
+++ jsdoc/processors/parse-tags.js
@@ -1,23 +1,130 @@
-var _ = require('lodash');
-var log = require('winston');
+require('es6-shim');
+var TagCollection = require('../lib/TagCollection');
+var Tag = require('../lib/Tag');
 
 /**
- * @dgProcessor parse-tags
- * @description Parse the doc for tags
+ * @dgProcessor parseTagsProcessor
+ * @description Parse the doc for jsdoc style tags
  */
-var plugin = module.exports = {
-  name: 'parse-tags',
-  runAfter: ['parsing-tags'],
-  runBefore: ['tags-parsed'],
-  process: function(docs, tagParser) {
+module.exports = function parseTagsProcessor(log) {
+  return {
+    tagDefinitions: [],
+    $validate: {
+      tagDefinitions: { presence: true }
+    },
+    $runAfter: ['parsing-tags'],
+    $runBefore: ['tags-parsed'],
+    $process: function(docs) {
 
-    _.forEach(docs, function(doc) {
-      try {
-        doc.tags = tagParser(doc.content, doc.startingLine);
-      } catch(e) {
-        log.error('Error parsing tags for doc starting at ' + doc.startingLine + ' in file ' + doc.file);
-        throw e;
-      }
-    });
-  }
+      var tagParser = createTagParser(this.tagDefinitions);
+
+      docs.forEach(function(doc) {
+        try {
+          doc.tags = tagParser(doc.content, doc.startingLine);
+        } catch(e) {
+          log.error('Error parsing tags for doc starting at ' + doc.startingLine + ' in file ' + doc.file);
+          throw e;
+        }
+      });
+    }
+  };
 };
+
+/**
+ * Create a map of the tagDefinitions keyed on their name or alias
+ * @param  {Array} tagDefinitions  A collection of tag definitions to map
+ * @return {Map}                [description]
+ */
+function createTagDefMap(tagDefinitions) {
+  // Create a map of the tagDefinitions so that we can look up tagDefs based on name or alias
+  var map = new Map();
+  tagDefinitions.forEach(function(tagDefinition) {
+    map.set(tagDefinition.name, tagDefinition);
+    if ( tagDefinition.aliases ) {
+      tagDefinition.aliases.forEach(function(alias) {
+        map.set(alias, tagDefinition);
+      });
+    }
+  });
+  return map;
+}
+
+/**
+ * Create a new tagParser that can parse a set of jsdoc-style tags from a document
+ * @param  {Array} tagDefMap A map of tag definitions keyed on tagName/aliasName.
+ */
+function createTagParser(tagDefinitions) {
+
+  var END_OF_LINE = /\r?\n/;
+  var TAG_MARKER = /^\s*@(\S+)\s*(.*)$/;
+  var CODE_FENCE = /^\s*```/;
+  var tagDefMap = createTagDefMap(tagDefinitions);
+
+  /**
+   * tagParser
+   * @param  {string} content      The text to parse for tags
+   * @param  {number} startingLine The line in the doc file where this text begins
+   * @return {TagCollection}       A collection of tags that were parsed
+   */
+  return function tagParser(content, startingLine) {
+    var lines = content.split(END_OF_LINE);
+    var lineNumber = 0;
+    var line, match, tagDef;
+    var descriptionLines = [];
+    var current;          // The current that that is being extracted
+    var inCode = false;   // Are we inside a fenced, back-ticked, code block
+    var tags = new TagCollection();        // Contains all the tags that have been found
+
+
+    // Extract the description block
+    do {
+      line = lines[lineNumber];
+
+      if ( CODE_FENCE.test(line) ) {
+        inCode = !inCode;
+      }
+
+      // We ignore tags if we are in a code block
+      match = TAG_MARKER.exec(line);
+      tagDef = match && tagDefMap.get(match[1]);
+      if ( !inCode && match && ( !tagDef || !tagDef.ignore ) ) {
+        // Only store tags that are unknown or not ignored
+        current = new Tag(tagDef, match[1], match[2], startingLine + lineNumber);
+        break;
+      }
+
+      lineNumber += 1;
+      descriptionLines.push(line);
+
+    } while(lineNumber < lines.length);
+    tags.description = descriptionLines.join('\n');
+
+    lineNumber += 1;
+
+    // Extract the tags
+    while(lineNumber < lines.length) {
+      line = lines[lineNumber];
+
+      if ( CODE_FENCE.test(line) ) {
+        inCode = !inCode;
+      }
+
+      // We ignore tags if we are in a code block
+      match = TAG_MARKER.exec(line);
+      tagDef = match && tagDefMap.get(match[1]);
+      if ( !inCode && match && (!tagDef || !tagDef.ignore) ) {
+        tags.addTag(current);
+        current = new Tag(tagDef, match[1], match[2], startingLine + lineNumber);
+      } else {
+        current.description = current.description ? (current.description + '\n' + line) : line;
+      }
+
+      lineNumber += 1;
+    }
+    if ( current ) {
+      tags.addTag(current);
+    }
+
+    return tags;
+  };
+}
--- jsdoc/spec/processors/parse-tags.spec.js
+++ jsdoc/spec/processors/parse-tags.spec.js
@@ -1,20 +1,92 @@
-var parseTagsProcessor = require('../../processors/parse-tags');
+var factory = require('../../processors/parse-tags');
+var mockLog = require('dgeni/lib/mocks/log')();
 
 describe("parse-tags processor", function() {
-  it("should be called 'parse-tags'", function() {
-    expect(parseTagsProcessor.name).toEqual('parse-tags');
-  });
-  it("should be run in the correct place", function() {
-    expect(parseTagsProcessor.runAfter).toEqual([ 'parsing-tags' ]);
-    expect(parseTagsProcessor.runBefore).toEqual([ 'tags-parsed' ]);
+  var processor;
+  var tagDefinitions = [
+    { name: 'id' },
+    { name: 'description' },
+    { name: 'param' },
+    { name: 'other-tag', ignore: true }
+  ];
+
+  beforeEach(function() {
+    processor = factory(mockLog);
+    processor.tagDefinitions = tagDefinitions;
   });
 
-  it("should call tagParser for each doc and assign the result to `doc.tags`", function() {
-    var testTag = {};
-    var tagParser = jasmine.createSpy('tagParser').and.returnValue([testTag]);
-    var doc = {};
-    parseTagsProcessor.process([doc], tagParser);
-    expect(tagParser).toHaveBeenCalled();
-    expect(doc.tags).toEqual([testTag]);
+  it("should have name the correct name", function() {
+    expect(factory.name).toEqual('parseTagsProcessor');
   });
+
+  it("should be run in the correct place", function() {
+    expect(processor.$runAfter).toEqual([ 'parsing-tags' ]);
+    expect(processor.$runBefore).toEqual([ 'tags-parsed' ]);
+  });
+
+
+  it("should only return tags that are not ignored", function() {
+    var content = 'Some initial content\n@id some.id\n' +
+                  '@description Some description\n@other-tag Some other tag\n' +
+                  '@param some param\n@param some other param';
+    var doc = {content: content, startingLine: 10};
+    processor.$process([doc]);
+
+    expect(doc.tags.tags[0]).toEqual(
+      jasmine.objectContaining({ tagName: 'id', description: 'some.id', startingLine: 11 })
+    );
+      // Not that the description tag contains what appears to be another tag but it was ignored so
+      // is consumed into the description tag!
+    expect(doc.tags.tags[1]).toEqual(
+      jasmine.objectContaining({ tagName: 'description', description: 'Some description\n@other-tag Some other tag', startingLine: 12})
+    );
+    expect(doc.tags.tags[2]).toEqual(
+      jasmine.objectContaining({ tagName: 'param', description: 'some param', startingLine: 14 })
+    );
+    expect(doc.tags.tags[3]).toEqual(
+      jasmine.objectContaining({ tagName: 'param', description: 'some other param', startingLine: 15 })
+    );
+  });
+
+    it("should cope with tags that have no 'description'", function() {
+      var content = '@id\n@description some description';
+      var doc = { content: content, startingLine: 123 };
+      processor.$process([doc]);
+      expect(doc.tags.tags[0]).toEqual(jasmine.objectContaining({ tagName: 'id', description: '' }));
+      expect(doc.tags.tags[1]).toEqual(jasmine.objectContaining({ tagName: 'description', description: 'some description' }));
+    });
+
+    it("should cope with empty content or no known tags", function() {
+      expect(function() {
+        processor.$process([{ content: '', startingLine: 123 }]);
+      }).not.toThrow();
+
+      expect(function() {
+        processor.$process([{ content: '@unknownTag some text', startingLine: 123 }]);
+      }).not.toThrow();
+    });
+
+
+    it("should ignore @tags inside back-ticked code blocks", function() {
+      processor.tagDefinitions = [{ name: 'a' }, { name: 'b' }];
+      var content =
+      '@a some text\n\n' +
+        '```\n' +
+        '  some code\n' +
+        '  @b not a tag\n' +
+        '```\n\n' +
+        'more text\n' +
+        '@b is a tag';
+      var doc = { content: content };
+      processor.$process([doc]);
+      expect(doc.tags.getTag('a').description).toEqual('some text\n\n' +
+        '```\n' +
+        '  some code\n' +
+        '  @b not a tag\n' +
+        '```\n\n' +
+        'more text'
+      );
+      expect(doc.tags.getTags('b').length).toEqual(1);
+      expect(doc.tags.getTag('b').description).toEqual('is a tag');
+    });
 });
