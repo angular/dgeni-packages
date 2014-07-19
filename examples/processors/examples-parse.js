fe17e2b8dd35931c41e1ad3174c8ca39f73bb62a
Peter Bacon Darwin <pete@bacondarwin.com>
2014-07-19 19:27:31 +0100

    refact(parseExamplesProcessor): update to no-config

--- examples/processors/examples-parse.js
+++ examples/processors/examples-parse.js
@@ -1,82 +1,85 @@
 var _ = require('lodash');
-var log = require('winston');
 var path = require('canonical-path');
-var trimIndentation = require('../../utils/trim-indentation');
-var marked = require('marked');
 
 var EXAMPLE_REGEX = /<example([^>]*)>([\S\s]+?)<\/example>/g;
 var ATTRIBUTE_REGEX = /\s*([^=]+)\s*=\s*(?:(?:"([^"]+)")|(?:'([^']+)'))/g;
 var FILE_REGEX = /<file([^>]*)>([\S\s]+?)<\/file>/g;
 
 
-function extractAttributes(attributeText) {
-  var attributes = Object.create(null);
-  attributeText.replace(ATTRIBUTE_REGEX, function(match, prop, val1, val2){
-    attributes[prop] = val1 || val2;
-  });
-  return attributes;
-}
+/**
+ * @dgProcessor parseExamplesProcessor
+ * @description
+ * Search the documentation for examples that need to be extracted
+ */
+module.exports = function parseExamplesProcessor(log, examples, trimIndentation) {
+  return {
+    outputFolder: undefined,
+    $runAfter: ['files-read'],
+    $runBefore: ['parsing-tags'],
+    $process: function(docs) {
 
-function extractFiles(exampleText) {
-  var files = Object.create(null);
-  exampleText.replace(FILE_REGEX, function(match, attributesText, contents) {
-    var file = extractAttributes(attributesText);
-    if ( !file.name ) {
-      throw new Error('Missing name attribute in file: ' + match);
-    }
+      var outputFolder = this.outputFolder || 'examples';
 
-    // Extract the contents of the file
-    file.fileContents = trimIndentation(contents);
-    file.language = path.extname(file.name).substr(1);
-    file.type = file.type || file.language || 'file';
-    file.attributes = _.omit(file, ['fileContents']);
+      docs.forEach(function(doc) {
+        doc.content = doc.content.replace(EXAMPLE_REGEX, function processExample(match, attributeText, exampleText) {
 
-    // Store this file information
-    files[file.name] = file;
-  });
-  return files;
-}
+          var example = extractAttributes(attributeText);
+          var id = uniqueName(examples, 'example-' + (example.name || 'example'));
+          _.assign(example, {
+            attributes: _.omit(example, ['files', 'doc']),
+            files: extractFiles(exampleText),
+            id: id,
+            doc: doc,
+            outputFolder: path.join(outputFolder, id)
+          });
 
-var uniqueName = function(container, name) {
-  if ( container[name] ) {
-    var index = 1;
-    while(container[name + index]) {
-      index += 1;
-    }
-    name = name + index;
-  }
-  return name;
-};
+          // store the example information for later
+          log.debug('Storing example', id);
+          examples.set(id, example);
 
-module.exports = {
-  name: 'examples-parse',
-  description: 'Search the documentation for examples that need to be extracted',
-  runAfter: ['files-read'],
-  runBefore: ['parsing-tags'],
-  exports: {
-    examples: ['value', Object.create(null) ]
-  },
-  process: function(docs, examples, config) {
-
-    var outputFolder = config.get('processing.examples.outputFolder', 'examples');
-
-    _.forEach(docs, function(doc) {
-      doc.content = doc.content.replace(EXAMPLE_REGEX, function processExample(match, attributeText, exampleText) {
-        var example = extractAttributes(attributeText);
-        example.attributes = _.omit(example, ['files', 'doc']);
-        var id = uniqueName(examples, 'example-' + (example.name || 'example'));
-        example.files = extractFiles(exampleText);
-        example.id = id;
-        example.doc = doc;
-        example.outputFolder = path.join(outputFolder, example.id);
-
-        // store the example information for later
-        log.debug('Storing example', id);
-        examples[id] = example;
-
-        return '{@runnableExample ' + id + '}';
+          return '{@runnableExample ' + id + '}';
+        });
       });
-    });
 
+    }
+  };
+
+  function extractAttributes(attributeText) {
+    var attributes = Object.create(null);
+    attributeText.replace(ATTRIBUTE_REGEX, function(match, prop, val1, val2){
+      attributes[prop] = val1 || val2;
+    });
+    return attributes;
+  }
+
+  function extractFiles(exampleText) {
+    var files = Object.create(null);
+    exampleText.replace(FILE_REGEX, function(match, attributesText, contents) {
+      var file = extractAttributes(attributesText);
+      if ( !file.name ) {
+        throw new Error('Missing name attribute in file: ' + match);
+      }
+
+      // Extract the contents of the file
+      file.fileContents = trimIndentation(contents);
+      file.language = path.extname(file.name).substr(1);
+      file.type = file.type || file.language || 'file';
+      file.attributes = _.omit(file, ['fileContents']);
+
+      // Store this file information
+      files[file.name] = file;
+    });
+    return files;
+  }
+
+  function uniqueName(container, name) {
+    if ( container.has(name) ) {
+      var index = 1;
+      while(container.has(name + index)) {
+        index += 1;
+      }
+      name = name + index;
+    }
+    return name;
   }
 };
--- examples/spec/processors/examples-parse.spec.js
+++ examples/spec/processors/examples-parse.spec.js
@@ -1,24 +1,19 @@
-var rewire = require('rewire');
-var plugin = rewire('../../processors/examples-parse');
-var Config = require('dgeni').Config;
-var log = require('winston');
+require('es6-shim');
+var parseExamplesProcessorFactory = require('../../processors/examples-parse');
+var mockLog = require('dgeni/lib/mocks/log')(false);
 var _ = require('lodash');
 
 describe("examples-parse doc processor", function() {
-  var config;
+
+  var processor, examples, mockTrimIndentation;
 
   beforeEach(function() {
-    config = new Config();
-    log.level = 'error';
+    examples = new Map();
+    mockTrimIndentation = jasmine.createSpy('trimIndentation').and.callFake(function(value) { return value; });
+    processor = parseExamplesProcessorFactory(mockLog, examples, mockTrimIndentation);
   });
-
-  it("should be called examples", function() {
-    expect(plugin.name).toEqual('examples-parse');
-  });
-
 
   it("should extract example tags from the doc content", function() {
-    var examples = {};
     var docs = [
       {
         content: 'a b c <example name="bar" moo1="nar1">some example content 1</example> x y z\n' +
@@ -31,15 +26,15 @@ describe("examples-parse doc processor", function() {
         content: '<example name="with-files"><file name="app.js">aaa</file><file name="app.spec.js" type="spec">bbb</file></example>'
       }
     ];
-    plugin.process(docs, examples, config);
-    expect(examples['example-bar']).toEqual(jasmine.objectContaining({ name:'bar', moo1:'nar1', id: 'example-bar'}));
-    expect(examples['example-bar1']).toEqual(jasmine.objectContaining({ name:'bar', moo2:'nar2', id: 'example-bar1'}));
-    expect(examples['example-value']).toEqual(jasmine.objectContaining({ name:'value', id: 'example-value'}));
-    expect(examples['example-with-files']).toEqual(jasmine.objectContaining({ name: 'with-files', id: 'example-with-files'}));
+    processor.$process(docs);
+    expect(examples.get('example-bar')).toEqual(jasmine.objectContaining({ name:'bar', moo1:'nar1', id: 'example-bar'}));
+    expect(examples.get('example-bar1')).toEqual(jasmine.objectContaining({ name:'bar', moo2:'nar2', id: 'example-bar1'}));
+    expect(examples.get('example-value')).toEqual(jasmine.objectContaining({ name:'value', id: 'example-value'}));
+    expect(examples.get('example-with-files')).toEqual(jasmine.objectContaining({ name: 'with-files', id: 'example-with-files'}));
 
     // Jasmine doesn't like that the files property hasn't got a hasOwnProperty method because it was created using Object.create(null);
     // So we map it into something else
-    var files = _.map(examples['example-with-files'].files, function(file) { return _.clone(file); });
+    var files = _.map(examples.get('example-with-files').files, function(file) { return _.clone(file); });
     expect(files).toEqual([
       jasmine.objectContaining({ name: 'app.js', type: 'js', fileContents: 'aaa', language: 'js' }),
       jasmine.objectContaining({ name: 'app.spec.js', type: 'spec', fileContents: 'bbb', language: 'js' })
@@ -48,14 +43,13 @@ describe("examples-parse doc processor", function() {
 
 
   it("should compute unique ids for each example", function() {
-    var examples = [];
     var docs = [{
       content: '<example name="bar">some example content 1</example>\n' +
                     '<example name="bar">some example content 2</example>'
     }];
-    plugin.process(docs, examples, config);
-    expect(examples['example-bar'].id).toEqual('example-bar');
-    expect(examples['example-bar1'].id).toEqual('example-bar1');
+    processor.$process(docs);
+    expect(examples.get('example-bar').id).toEqual('example-bar');
+    expect(examples.get('example-bar1').id).toEqual('example-bar1');
   });
 
   it("should inject a new set of elements in place of the example into the original markup to be used by the template", function() {
@@ -63,7 +57,7 @@ describe("examples-parse doc processor", function() {
       content: 'Some content before <example name="bar">some example content 1</example> and some after'
     };
 
-    plugin.process([doc], [], config);
+    processor.$process([doc]);
 
     expect(doc.content).toEqual('Some content before {@runnableExample example-bar} and some after');
 
