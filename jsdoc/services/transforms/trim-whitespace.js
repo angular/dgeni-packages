63a05e2356bf14ea260e677d0bf49b8f3b70012b
Peter Bacon Darwin <pete@bacondarwin.com>
2014-07-21 15:03:26 +0100

    fix(trimWhitespaceTransform): function name had bad capitalization

--- jsdoc/services/transforms/trim-whitespace.js
+++ jsdoc/services/transforms/trim-whitespace.js
@@ -3,7 +3,7 @@ var _ = require('lodash');
 /**
  * Trim excess whitespace from the value
  */
-module.exports = function trimWhiteSpaceTransform() {
+module.exports = function trimWhitespaceTransform() {
   return function(doc, tag, value) {
     if ( _.isString(value) ) {
       return value.trim();
