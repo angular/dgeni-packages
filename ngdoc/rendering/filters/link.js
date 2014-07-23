43a5604b6be326068e90f0943cd816e3cca852da
Peter Bacon Darwin <pete@bacondarwin.com>
2014-07-23 17:26:33 +0100

    fix(ngdoc/link filter): convert to DI injectable

--- ngdoc/rendering/filters/link.js
+++ ngdoc/rendering/filters/link.js
@@ -1,8 +1,10 @@
 var _ = require('lodash');
 
-module.exports = {
-  name: 'link',
-  process: function(url, title, doc) {
-    return _.template('{@link ${url} ${title} }', { url: url, title: title });
-  }
+module.exports = function() {
+  return {
+    name: 'link',
+    process: function(url, title, doc) {
+      return _.template('{@link ${url} ${title} }', { url: url, title: title });
+    }
+  };
 };
--- ngdoc/spec/rendering/filters/link.spec.js
+++ ngdoc/spec/rendering/filters/link.spec.js
@@ -1,6 +1,11 @@
-var filter = require('../../../rendering/filters/link');
+var filterFactory = require('../../../rendering/filters/link');
 
 describe("link filter", function() {
+  var filter;
+
+  beforeEach(function() {
+    filter = filterFactory();
+  });
 
   it("should have the name 'link'", function() {
     expect(filter.name).toEqual('link');
