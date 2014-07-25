2bfa92b2b12732b60f37daed856d1ed94a6b9c0b
Peter Bacon Darwin <pete@bacondarwin.com>
2014-07-25 09:27:56 +0100

    feat(dgeni package): add initial package for documenting dgeni configurations

---
+++ dgeni/index.js
@@ -0,0 +1,14 @@
+var path = require('canonical-path');
+var packagePath = __dirname;
+var Package = require('dgeni').Package;
+
+module.exports = new Package('dgeni', ['jsdoc'])
+
+.config(function(parseTagsProcessor, getInjectables) {
+  parseTagsProcessor.tagDefinitions = parseTagsProcessor.tagDefinitions.concat(
+    getInjectables([
+      require('./tag-defs/dgPackage'),
+      require('./tag-defs/dgService'),
+      require('./tag-defs/dgProcessor')
+    ]));
+});
