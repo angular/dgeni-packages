4b4eeb9e588b1d324b5bc3582e87cf34163a3096
Peter Bacon Darwin <pete@bacondarwin.com>
2014-09-01 14:17:01 +0100

    test(base/debugDumpProcessor): use configureInjector and mockPackage

--- base/spec/processors/debugDumpProcessor.spec.js
+++ base/spec/processors/debugDumpProcessor.spec.js
@@ -3,16 +3,22 @@ var path = require('canonical-path');
 var Q = require('q');
 var processorFactory = rewire('../../processors/debugDumpProcessor');
 
+var mockPackage = require('dgeni-packages/base/spec/mockPackage');
+var Dgeni = require('dgeni');
+
 var writeFileSpy = jasmine.createSpy('writeFile').and.returnValue(Q());
 processorFactory.__set__('writeFile', writeFileSpy);
 
-var mockLog = require('dgeni/lib/mocks/log')(/* true */);
 var mockReadFilesProcessor = {
   basePath: path.resolve('some/path')
 };
 
 describe("debugDumpProcessor", function() {
   it("should write out the docs to a file", function() {
+    var dgeni = new Dgeni([mockPackage()]);
+    var injector = dgeni.configureInjector();
+    var processor = injector.get('debugDumpProcessor');
+
     var processor = processorFactory(mockLog, mockReadFilesProcessor);
     processor.outputPath = 'build/dump.txt';
     processor.$process([{ val: 'a' }, { val: 'b' }]);
