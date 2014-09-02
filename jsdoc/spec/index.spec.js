9d08564b164ff56ed7c20082afaacb792db2b458
Peter Bacon Darwin <pete@bacondarwin.com>
2014-09-02 16:28:51 +0100

    test(jsdoc): use mockPackage

--- jsdoc/spec/index.spec.js
+++ jsdoc/spec/index.spec.js
@@ -1,21 +1,19 @@
-var jsdocPackage = require('../index');
+var mockPackage = require('dgeni-packages/jsdoc/spec/mockPackage');
 var Dgeni = require('dgeni');
-var mockLog = require('dgeni/lib/mocks/log');
 
 describe('jsdoc package', function() {
   it("should be instance of Package", function() {
-      expect(jsdocPackage instanceof Dgeni.Package).toBeTruthy();
+      expect(require('dgeni-packages/jsdoc') instanceof Dgeni.Package).toBeTruthy();
   });
 
   describe("computePathsProcessor", function() {
 
     function runDgeni(docs) {
-      var testPackage = new Dgeni.Package('testPackage', [jsdocPackage])
-        .factory('templateEngine', function() {})
-        .factory('log', function() { return mockLog(false); })
+      var testPackage = new Dgeni.Package('testPackage', [mockPackage()])
+
         .processor('provideTestDocs', function() {
           return {
-            $runBefore: ['computePathsProcessor'],
+            $runBefore: ['files-read'],
             $process: function() {
               return docs;
             }
@@ -25,7 +23,6 @@ describe('jsdoc package', function() {
         .config(function(readFilesProcessor, writeFilesProcessor, computeIdsProcessor, renderDocsProcessor, unescapeCommentsProcessor) {
           readFilesProcessor.$enabled = false;
           writeFilesProcessor.$enabled = false;
-          computeIdsProcessor.$enabled = false;
           renderDocsProcessor.$enabled = false;
           unescapeCommentsProcessor.$enabled = false;
         });
@@ -34,29 +31,37 @@ describe('jsdoc package', function() {
     }
 
 
-    it("should compute the path of the document from its name or codeName", function(done) {
-      var doc1 = { docType: 'js', fileInfo: { relativePath: 'a/b/c/foo.ngdoc', baseName: 'foo' }, name: 'fooName', codeName: 'fooCodeName' };
-      var doc2 = { docType: 'js', fileInfo: { relativePath: 'x/y/z/index.html', baseName: 'index' }, codeName: 'xyz' };
+    it("should compute the id and path of the document from its name or codeName", function(done) {
+      var doc1 = { docType: 'js', fileInfo: { relativePath: 'a/b/c/foo.ngdoc', baseName: 'foo' }, name: 'fooName', codeName: 'fooCodeName', content: 'some content' };
+      var doc2 = { docType: 'js', fileInfo: { relativePath: 'x/y/z/index.html', baseName: 'index' }, codeName: 'xyz', content: 'some content' };
 
         runDgeni([doc1,doc2]).then(function(docs) {
+          expect(doc1.id).toEqual('fooName');
+          expect(doc2.id).toEqual('xyz');
           expect(doc1.path).toEqual('fooName');
           expect(doc1.outputPath).toEqual('fooName.html');
           expect(doc2.path).toEqual('xyz');
           expect(doc2.outputPath).toEqual('xyz.html');
           done();
+        }).catch(function(error) {
+          console.log(error);
+          done();
         });
     });
 
 
-    it("should compute the path of the document from its file name", function(done) {
-      var doc1 = { docType: 'js', fileInfo: { relativePath: 'a/b/c/foo.ngdoc', baseName: 'foo' } };
-      var doc2 = { docType: 'js', fileInfo: { relativePath: 'x/y/z/index.html', baseName: 'index' } };
+    it("should compute the id and path of the document from its file name", function(done) {
+      var doc1 = { docType: 'js', fileInfo: { relativePath: 'a/b/c/foo.ngdoc', baseName: 'foo' }, content: 'some content'  };
+      var doc2 = { docType: 'js', fileInfo: { relativePath: 'x/y/z/index.html', baseName: 'index' }, content: 'some content'  };
 
         runDgeni([doc1,doc2]).then(function(docs) {
           expect(doc1.path).toEqual('a/b/c/foo');
           expect(doc1.outputPath).toEqual('a/b/c/foo.html');
           expect(doc2.path).toEqual('x/y/z');
           expect(doc2.outputPath).toEqual('x/y/z.html');
+          done();
+        }).catch(function(error) {
+          console.log(error);
           done();
         });
     });
