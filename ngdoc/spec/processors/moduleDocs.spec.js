3e42370c781f83ceabca7025df4be94ad9b0c5ae
Peter Bacon Darwin <pete@bacondarwin.com>
2014-09-05 14:41:10 +0100

    test(ngdoc/moduleDocsProcessor): test the processor

---
+++ ngdoc/spec/processors/moduleDocs.spec.js
@@ -0,0 +1,47 @@
+var mockPackage = require('dgeni-packages/ngdoc/spec/mockPackage');
+var Dgeni = require('dgeni');
+
+describe("moduleDocsProcessor", function() {
+  var processor, aliasMap;
+
+  beforeEach(function() {
+    var dgeni = new Dgeni([mockPackage()]);
+    var injector = dgeni.configureInjector();
+    processor = injector.get('moduleDocsProcessor');
+    moduleMap = injector.get('moduleMap');
+    aliasMap = injector.get('aliasMap');
+  });
+
+  it("should add module docs to the moduleMap", function() {
+    var doc1 = { docType: 'module', id: 'ng' };
+    var doc2 = { docType: 'module', id: 'ngMock' };
+    var doc3 = { docType: 'service', module: 'ng', id: 'ng.$http' };
+
+    processor.$process([doc1, doc2]);
+
+    expect(moduleMap.values().length).toEqual(2);
+    expect(moduleMap.get('ng')).toBe(doc1);
+    expect(moduleMap.get('ngMock')).toBe(doc2);
+  });
+
+  it("should connect all docs to their module", function() {
+    var doc1 = { docType: 'module', id: 'ng', aliases: ['ng'] };
+    var doc2 = { docType: 'module', id: 'ngMock', aliases: ['ngMock'] };
+    var doc3 = { docType: 'service', module: 'ng', id: 'ng.$http' };
+    var doc4 = { docType: 'service', module: 'ng', id: 'ng.$log' };
+    var doc5 = { docType: 'service', module: 'ngMock', id: 'ng.$log' };
+
+    aliasMap.addDoc(doc1);
+    aliasMap.addDoc(doc2);
+    processor.$process([doc1, doc2, doc3, doc4, doc5]);
+
+    expect(doc1.components).toEqual([doc3, doc4]);
+    expect(doc2.components).toEqual([doc5]);
+
+    expect(doc3.moduleDoc).toBe(doc1);
+    expect(doc4.moduleDoc).toBe(doc1);
+    expect(doc5.moduleDoc).toBe(doc2);
+
+  });
+
+});
