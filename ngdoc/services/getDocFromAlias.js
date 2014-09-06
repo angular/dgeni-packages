f742f5a3394510ce72db44ac03914a93273feb01
Peter Bacon Darwin <pete@bacondarwin.com>
2014-09-06 20:17:17 +0100

    fix(ngdoc/getDocFromAlias): start from scratch if filtering by area fails

--- ngdoc/services/getDocFromAlias.js
+++ ngdoc/services/getDocFromAlias.js
@@ -10,10 +10,15 @@ module.exports = function getDocFromAlias(aliasMap, log) {
     var docs = aliasMap.getDocs(alias);
 
     // If there is more than one item with this name then try to filter them by the originatingDoc's area
-    if ( docs.length > 1 && originatingDoc ) {
+    if ( docs.length > 1 && originatingDoc && originatingDoc.area) {
       docs = _.filter(docs, function(doc) {
         return doc.area === originatingDoc.area;
       });
+    }
+
+    // If filtering by area left us with none then let's start again
+    if ( docs.length === 0 ) {
+      docs = aliasMap.getDocs(alias);
     }
 
     // If there is more than one item with this name then try to filter them by the originatingDoc's module
