69680361ca412be0ac12c7afe63161b8b6ab4b92
Peter Bacon Darwin <pete@bacondarwin.com>
2014-10-15 17:29:46 +0100

    fix(jsdoc/jsdocFileReader): provide file information in JS parse error
    Closes #84

--- jsdoc/file-readers/jsdoc.js
+++ jsdoc/file-readers/jsdoc.js
@@ -7,16 +7,22 @@ var esprima = require('esprima');
  * This file reader will create a simple doc for each js
  * file including a code AST of the JavaScript in the file.
  */
-module.exports = function jsdocFileReader() {
+module.exports = function jsdocFileReader(log) {
   return {
     name: 'jsdocFileReader',
     defaultPattern: /\.js$/,
     getDocs: function(fileInfo) {
 
-      fileInfo.ast = esprima.parse(fileInfo.content, {
-        loc: true,
-        attachComment: true
-      });
+      try {
+        fileInfo.ast = esprima.parse(fileInfo.content, {
+          loc: true,
+          attachComment: true
+        });
+      } catch(ex) {
+       ex.file = fileInfo.filePath;
+        throw new Error(
+          _.template('JavaScript error in file "${file}"" [line ${lineNumber}, column ${column}]: "${description}"', ex));
+      }
 
       return [{
         docType: 'jsFile'
