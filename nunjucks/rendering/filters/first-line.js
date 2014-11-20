c11dd19c8cbee9092809c29e151ec0be90e0df2d
k-kinzal <kinzal@logn.in>
2014-11-21 00:20:59 +0900

    fixes get first line by regexp https://github.com/angular/dgeni-packages/pull/98#discussion_r20648929

--- nunjucks/rendering/filters/first-line.js
+++ nunjucks/rendering/filters/first-line.js
@@ -6,16 +6,9 @@ module.exports = {
   name: 'firstLine',
   process: function(str) {
     if (!str) return str;
-
-    var firstLine = str
-      .split("\n")[0];
-    if (firstLine.split("{@").length > firstLine.split("}").length) {
-      var match = str.match(/\n(.*?\})/);
-      if (match) {
-        firstLine = firstLine + ' ' + match[1];
-      }
-    }
-
-    return firstLine;
+    
+    return str.match(/([^$]*\{@[^}]+\})|.*(\n|$)/)[0]
+              .replace("\n", " ")
+              .trim();
   }
 };
--- nunjucks/rendering/filters/first-line.spec.js
+++ nunjucks/rendering/filters/first-line.spec.js
@@ -4,6 +4,9 @@ describe("firstLine filter", function() {
   it("should have the name 'firstLine'", function() {
     expect(filter.name).toEqual('firstLine');
   });
+  it("should return all at one line", function() {
+    expect(filter.process('this is a line')).toEqual('this is a line');
+  });
   it("should return the content up to the first newline", function() {
     expect(filter.process('this is a line\nthis is another line')).toEqual('this is a line');
   });
