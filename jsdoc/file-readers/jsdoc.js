313a8f012460e62d2c7f2506f783242a632b7e9e
forresst <forresst@voila.fr>
2015-05-06 10:52:03 +0200

    feat(jsdoc) : switch from Esprima to Espree

--- jsdoc/file-readers/jsdoc.js
+++ jsdoc/file-readers/jsdoc.js
@@ -1,5 +1,37 @@
 var _ = require('lodash');
-var esprima = require('esprima');
+var espree = require('espree');
+// exported so we can use them in tests
+var parserOptions = exports.parserOptions = {
+    comment: true,
+    loc: true,
+    range: true,
+    tokens: true,
+    ecmaFeatures: {
+        arrowFunctions: true,
+        binaryLiterals: true,
+        blockBindings: true,
+        classes: true,
+        defaultParams: true,
+        destructuring: true,
+        forOf: true,
+        generators: true,
+        globalReturn: true,
+        jsx: false,
+        modules: true,
+        objectLiteralComputedProperties: true,
+        objectLiteralDuplicateProperties: true,
+        objectLiteralShorthandMethods: true,
+        objectLiteralShorthandProperties: true,
+        octalLiterals: true,
+        regexUFlag: true,
+        regexYFlag: true,
+        restParams: true,
+        spread: true,
+        superInFunctions: true,
+        templateStrings: true,
+        unicodeCodePointEscapes: true
+    }
+};
 
 /**
  * @dgService jsdocFileReader
@@ -14,10 +46,7 @@ module.exports = function jsdocFileReader(log) {
     getDocs: function(fileInfo) {
 
       try {
-        fileInfo.ast = esprima.parse(fileInfo.content, {
-          loc: true,
-          attachComment: true
-        });
+        fileInfo.ast = espree.parse(fileInfo.content, parserOptions);
       } catch(ex) {
        ex.file = fileInfo.filePath;
         throw new Error(
