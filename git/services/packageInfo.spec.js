a70ae8de4d5863ae7f3bfb95b989e1e23fa3ff6f
Jacob Eggers <eggers@brandeis.edu>
2015-06-03 01:43:24 -0700

    feat(git) use spyOn to fix errors with coverage

--- git/services/packageInfo.spec.js
+++ git/services/packageInfo.spec.js
@@ -8,21 +8,20 @@ describe("packageInfo", function() {
   beforeEach(function() {
     fs = packageInfoFactory.__get__('fs');
     path = packageInfoFactory.__get__('path');
-    path.resolve = function() { return '' }
+    spyOn(path, 'resolve').and.returnValue('');
+    spyOn(fs, 'existsSync').and.returnValue(true);
   });
 
   it('should read package.json as UTF-8', function() {
-    fs.existsSync = function() { return true };
     spyOn(fs, 'readFileSync').and.returnValue('{}');
 
     packageInfoFactory();
 
     expect(fs.readFileSync).toHaveBeenCalledWith('package.json', 'UTF-8');
   });
-
   it('should return parsed file contents', function() {
     fs.existsSync = function() { return true };
-    fs.readFileSync = function() { return '{"foo":"bar"}' };
+    spyOn(fs, 'readFileSync').and.returnValue('{"foo":"bar"}');
 
     var packageInfo = packageInfoFactory();
 
@@ -30,19 +29,16 @@ describe("packageInfo", function() {
   });
 
   it('should walk up the tree looking for jasmine', function() {
-    fs.existsSync = function(file) {
+    fs.existsSync.and.callFake(function(file) {
       if (file == 'package.json') {
         return false;
       } else {
         return true;
       }
-    }
+    });
 
     spyOn(fs, 'readFileSync').and.returnValue('{}');
-
-    path.dirname = function() {
-      return '../';
-    }
+    spyOn(path, 'dirname').and.returnValue('../');
 
     packageInfoFactory();
 
