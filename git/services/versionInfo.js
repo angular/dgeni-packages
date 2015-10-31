069c77628352258eec657f42fff1e610b9fd8822
Peter Bacon Darwin <pete@bacondarwin.com>
2015-10-31 16:37:42 +0000

    fix(versionInfo): don't crash if no codename match is found
    Closes #140

--- git/services/versionInfo.js
+++ git/services/versionInfo.js
@@ -28,7 +28,8 @@ var satisfiesVersion = function(version) {
  */
 var getCodeName = function(tagName) {
   var gitCatOutput = shell.exec('git cat-file -p ' + tagName, {silent:true}).output;
-  var tagMessage = gitCatOutput.match(/^.*codename.*$/mg)[0];
+  var match = gitCatOutput.match(/^.*codename.*$/mg);
+  var tagMessage = match && match[0];
   var codeName = tagMessage && tagMessage.match(/codename\((.*)\)/)[1];
   if (!codeName) {
     throw new Error("Could not extract release code name. The message of tag " + tagName +
