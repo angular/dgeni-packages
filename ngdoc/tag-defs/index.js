b590d058f57adbdfa9eef6161083ee4f77c055b9
Peter Bacon Darwin <pete@bacondarwin.com>
2016-03-30 11:26:06 +0100

    feat(ngdoc): add knownIssues tag and processing
    Closes #163

--- ngdoc/tag-defs/index.js
+++ ngdoc/tag-defs/index.js
@@ -14,5 +14,6 @@ module.exports = [
   require('./parent'),
   require('./packageName'),
   require('./scope'),
-  require('./multiElement')
+  require('./multiElement'),
+  require('./knownIssue')
 ];
