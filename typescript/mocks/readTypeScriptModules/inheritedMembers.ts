538e8cd03e6a6e454c8529b81c49b138235e3c41
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-03-04 19:12:02 +0100

    Add test interface

--- typescript/mocks/readTypeScriptModules/inheritedMembers.ts
+++ typescript/mocks/readTypeScriptModules/inheritedMembers.ts
@@ -1,8 +1,8 @@
-export class LastParent {
+export class LastParent implements TestInterface {
   lastParentProp: number = 0;
 }
 
-export class Child extends FirstParent {
+export class Child extends FirstParent implements TestInterface {
   childProp: boolean = false;
 }
 
@@ -10,8 +10,14 @@ export class Child extends FirstParent {
  * To ensure that Dgeni properly recognizes classes from exports that will be parsed later,
  * the `FirstParent` class will be ordered after the `Child` class.
  **/
-export class FirstParent extends LastParent {
+export class FirstParent extends LastParent implements TestInterface {
   firstParentProp: string = 'Works';
   _privateProp: string = 'Private';
   private privateProp: string = 'Private';
 }
+
+/**
+ * This is just a test interface that has been added to ensure that Dgeni only recognizes
+ * extends heritages.
+ **/
+export interface TestInterface {}
