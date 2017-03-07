0f48e866d4dc8d4e9a9ec96a52dbf163b9d57898
Peter Bacon Darwin <pete@bacondarwin.com>
2017-03-07 13:44:29 +0000

    feat(typescript): upgrade to TypeScript 2.2.1

---
+++ typescript/mocks/readTypeScriptModules/commentContent.ts
@@ -0,0 +1,18 @@
+/**
+ * @empty
+ */
+export class SomeClass {
+  /**
+   * The description
+   * @tag1
+   * tag info
+   */
+  foo: string;
+
+  /**
+   * @name bar
+   * @description
+   * description of bar {@inline-tag} more content
+   */
+  bar(value: any): string;
+}
