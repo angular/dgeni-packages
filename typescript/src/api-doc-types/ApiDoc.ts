a96184ac58c0450c8e6ba790cac2ea9115cfb872
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-02-24 12:03:54 +0100

    feat(typescript): create `BaseApiDoc` class

--- typescript/src/api-doc-types/ApiDoc.ts
+++ typescript/src/api-doc-types/ApiDoc.ts
@@ -1,6 +1,7 @@
-import { Declaration, Symbol } from 'typescript';
-import { AugmentedSymbol } from '../services/TsParser';
+import { Declaration, Symbol, TypeChecker } from 'typescript';
 import { FileInfo } from '../services/TsParser/FileInfo';
+import { getContent } from '../services/TsParser/getContent';
+import { ModuleDoc } from './ModuleDoc';
 
 export interface ApiDoc {
   docType: string;
@@ -15,4 +16,32 @@ export interface ApiDoc {
   fileInfo: FileInfo;
   startingLine: number;
   endingLine: number;
+}
+
+export abstract class BaseApiDoc implements ApiDoc {
+
+  // Concrete implementations will provide the docType string
+  abstract docType: string;
+
+  name = this.aliasSymbol ? this.aliasSymbol.name : this.symbol.name;
+  aliases = [this.name, this.moduleDoc.id + "/" + this.name];
+  id = this.moduleDoc.id + "/" + this.name;
+  basePath = this.moduleDoc.basePath;
+  fileInfo = new FileInfo(this.declaration, this.basePath);
+  startingLine = this.fileInfo.location.start.line +
+    (this.fileInfo.location.start.character ? 1 : 0);
+  endingLine = this.fileInfo.location.end.line;
+  content = getContent(this.declaration);
+  path: string;
+  outputPath: string;
+
+  originalModule = this.fileInfo.projectRelativePath
+    .replace(new RegExp("\." + this.fileInfo.extension + "$"), "");
+  namespacesToInclude = this.moduleDoc.namespacesToInclude;
+  typeChecker: TypeChecker = this.moduleDoc.typeChecker;
+
+  constructor(public moduleDoc: ModuleDoc,
+              public symbol: Symbol,
+              public declaration: Declaration,
+              public aliasSymbol?: Symbol) {}
 }
