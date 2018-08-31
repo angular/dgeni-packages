da3323f7be58a006d8742cc89d2b6b8156d60af8
Pete Bacon Darwin <pete@bacondarwin.com>
2018-08-31 15:34:25 +0100

    feat(typescript): improve declaration type text rendering
    Where a declaration is an initializer and its value is a
    new instance of a class, then display the type of that
    class.

--- typescript/src/services/TsParser/getDeclarationTypeText.ts
+++ typescript/src/services/TsParser/getDeclarationTypeText.ts
@@ -1,26 +1,41 @@
-import { Declaration, Node, SyntaxKind, TypeNode, TypeParameterDeclaration } from 'typescript';
+import * as ts from 'typescript';
 import { getTypeText } from './getTypeText';
 import { nodeToString } from './nodeToString';
 
-export function getDeclarationTypeText(declaration: Declaration) {
+export function getDeclarationTypeText(declaration: ts.Declaration): string {
   // if the declaration has an explicit type then use that
   const type = getType(declaration);
   if (type) return getTypeText(type);
 
   // if the declaration is a type parameter then just use its textual value
-  if (declaration.kind === SyntaxKind.TypeParameter ) {
+  if (ts.isTypeParameterDeclaration(declaration)) {
     return nodeToString(declaration);
   }
 
   // if the declaration is being initialized then use the initialization value
   const initializer = getInitializer(declaration);
-  return initializer ? nodeToString(initializer) : '';
+  if (initializer) {
+    if (ts.isNewExpression(initializer)) {
+      return nodeToString(initializer.expression) + getTypeArgumentText(initializer);
+    } else {
+      return nodeToString(initializer);
+    }
+  }
+  return '';
 }
 
-function getType(declaration: Declaration) {
-  return (declaration as any).type as TypeNode;
+function getType(declaration: ts.Declaration) {
+  return (declaration as any).type as ts.TypeNode;
 }
 
-export function getInitializer(declaration: Declaration) {
-  return (declaration as any).initializer as Node;
+function getInitializer(declaration: ts.Declaration): ts.Expression|undefined {
+  return (declaration as any).initializer;
+}
+
+function getTypeArgumentText(initializer: ts.NewExpression) {
+  if (!initializer.typeArguments) {
+    return '';
+  }
+  const typeTexts = initializer.typeArguments.map(t => nodeToString(t));
+  return `<${ typeTexts.join(', ')}>`;
 }
