ed275635adc5f2a6b2b82b26b3150fdb8aaddc36
Rodolfo Aguirre <aguirreg.rodolfo@gmail.com>
2019-11-05 18:16:01 -0600

    fix(git): use correct shell execution status property
    The previous migration from `shelljs` to `child_process` in commit
    18a1c0b0692c05d43126150f1d8e284850ea2da3 failed to update
    some usage of the return status properties from `.code` to `.status`.

--- git/mocks/mocks.js
+++ git/mocks/mocks.js
@@ -26,39 +26,39 @@ var mockVersionInfo = {
 };
 
 var mockGitLsRemoteTags = {
-  code: 0,
+  status: 0,
   stdout: '85ae09c2119bf9b20cd45fc4e9dab77c5940d627	refs/tags/v0.10.11-rc2\n' +
           '373c3bf61785139a65e76c023e798b49b7437c37	refs/tags/v0.10.13\n' +
           '573c3bf61795139a65e76c023e798b49b7437c37	refs/tags/v1.3.invalid'
 };
 
 var mockGitRevParse = {
-  code: 0,
+  status: 0,
   stdout: 'revision'
 }
 
 var mockGitCatFile = {
-  code: 0,
+  status: 0,
   stdout: 'codename(mockCodeName)'
 }
 
 var mockGitCatFileNoCodeName = {
-  code: 0,
+  status: 0,
   stdout: 'no code name'
 }
 
 var mockGitCatFileBadFormat = {
-  code: 0,
+  status: 0,
   stdout: 'bad format codename()'
 }
 
 var mockGitDescribe = {
-  code: 0,
+  status: 0,
   stdout: 'v0.10.15'
 };
 
 var mockDefaultFail = {
-  code: 1,
+  status: 1,
   stdout: "default"
 };
 
